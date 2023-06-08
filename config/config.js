f75166cf998fc7bd55fab0744e463dcb195c9160
sweetNana <dyahnahdya@gmail.com>
2023-06-08 15:28:41 +0700

    penambahan .env

--- config/config.js
+++ config/config.js
@@ -2,19 +2,19 @@
 const mysql2 = require("mysql2");
 
 const db = mysql2.createConnection({
-    // host: process.env.HOST,
-    // user: process.env.USER,
-    // password: process.env.PW,
-    // database: process.env.DB,
-    // socketPath: process.env.SOCKETPATH,
-    // multipleStatements: true,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PW,
+    database: process.env.DB,
+    //socketPath: process.env.SOCKETPATH,
+    multipleStatements: true,
 
     // dev
-    host: "34.101.201.55",
-    user: "root",
-    password: "octofishdb",
-    database: "octofish-db",
-    multipleStatements: true,
+    // host: "34.101.201.55",
+    // user: "root",
+    // password: "octofishdb",
+    // database: "octofish-db",
+    // multipleStatements: true,
 
 });
 
